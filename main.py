4c190a5a1f2c846ee5cd1929e173f010431ae50d
ds111g <ds111g@att.com>
2021-05-03 14:46:16 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,46 @@
+import logging
+import os
+
+from flask import Flask, request, jsonify, send_from_directory
+
+from temp_sensor import TempSensor
+from properties import ip, port
+
+app = Flask(__name__)
+
+# create logger with 'spam_application'
+logger = logging.getLogger('main')
+logger.setLevel(logging.DEBUG)
+# create file handler which logs even debug messages
+fh = logging.FileHandler('log.log')
+fh.setLevel(logging.DEBUG)
+# create console handler with a higher log level
+ch = logging.StreamHandler()
+ch.setLevel(logging.ERROR)
+# create formatter and add it to the handlers
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+ch.setFormatter(formatter)
+# add the handlers to the logger
+logger.addHandler(fh)
+logger.addHandler(ch)
+
+sensor = TempSensor()
+
+
+@app.route('/getTemp')
+def get_temp():
+    ret = {"temp": sensor.temp,
+           "humidity": sensor.humidity}
+    return ret, 200
+
+
+if __name__ == '__main__':
+    try:
+        stream = os.popen('hostname -I')
+        host_name = stream.read().strip()
+    except all:
+        host_name = ip
+    logger.info("machine host_name[" + host_name + "]")
+    sensor.start()
+    app.run(host=host_name, port=port)
---
+++ temp_sensor.py
@@ -0,0 +1,45 @@
+import threading
+import smbus
+import time
+
+bus = smbus.SMBus(1)
+config = [0x08, 0x00]
+
+
+class TempSensor(object):
+    def __init__(self):
+        self._temp = 0.0
+        self._humidity = 0.0
+        self._running = False
+
+    def read_sensor(self):
+        bus.write_i2c_block_data(0x38, 0xE1, config)
+        byt = bus.read_byte(0x38)
+        measure_cmd = [0x33, 0x00]
+        bus.write_i2c_block_data(0x38, 0xAC, measure_cmd)
+        time.sleep(0.5)
+        data = bus.read_i2c_block_data(0x38, 0x00)
+        temp_raw = ((data[3] & 0x0F) << 16) | (data[4] << 8) | data[5]
+        temp_c = ((temp_raw * 200) / 1048576) - 50
+        humid_raw = ((data[1] << 16) | (data[2] << 8) | data[3]) >> 4
+        humid = humid_raw * 100 / 1048576
+        self._temp = round(temp_c, 2)
+        self._humidity = round(humid, 1)
+        if self._running:
+            timer = threading.Timer(15, self.read_sensor)
+            timer.start()
+
+    def start(self):
+        self._running = True
+        self.read_sensor()
+
+    def stop(self):
+        self._running = True
+
+    @property
+    def temp(self):
+        return self._temp
+
+    @property
+    def humidity(self):
+        return self._humidity
